42b088ac37dd7b0a8fc6c84979db790ca919b373
Robear <rob@robmagee.com.au>
2018-12-08 09:35:50 +0000

    modif: wall_follower cmakelists.txt build file

--- catkin_ws/src/ShellScripts/test_slam.sh
+++ catkin_ws/src/ShellScripts/test_slam.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
-sleep 8
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
-sleep 8
+sleep 5
+xterm  -e  " roslaunch home_service_bot MyWorld.launch " &
+sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
--- catkin_ws/src/ShellScripts/test_world.sh
+++ catkin_ws/src/ShellScripts/test_world.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
 sleep 5
-xterm  -e  "rosrun gazebo /src/home_service_bot/world/MyWorld/MyWorld.world" 
+xterm  -e  "roslaunch gazebo /src/home_service_bot/worlds/MyWorld.world" 
 
